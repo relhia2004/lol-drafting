c0e13efd41a0c2c69b5fa25ee5794de162f2c47a
Aloit <relhia2004@gmail.com>
2026-07-31 18:43:24 +0800

    Configure Supabase JS SDK client credentials

--- api/db.ts
+++ api/db.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Reads standard Supabase HTTP URL and Anon Key
 const supabaseUrl = process.env.SUPABASE_URL || 'https://ufedhnjvpklnunzckqma.supabase.co';
-const supabaseKey = process.env.SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
+const supabaseKey = process.env.SUPABASE_ANON_KEY || 'sb_publishable_T1-18efK-gCIgEVj5_u1mw_akNGdvLt';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
