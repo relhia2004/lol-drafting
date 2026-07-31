e5e3c94629a8c51b364c4a385f580280b481e297
Aloit <relhia2004@gmail.com>
2026-07-31 19:14:04 +0800

    test

--- api/db.ts
+++ api/db.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Reads standard Supabase HTTP URL and Anon Key
 const supabaseUrl = process.env.SUPABASE_URL || 'https://ufedhnjvpklnunzckqma.supabase.co';
-const supabaseKey = process.env.SUPABASE_ANON_KEY || 'sb_publishable_T1-18efK-gCIgEVj5_u1mw_akNGdvLt';
+const supabaseKey = process.env.SUPABASE_ANON_KEY || 'sb_secret_NDjaondUU8bamfKrIsR5HQ_df9djj7A';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
